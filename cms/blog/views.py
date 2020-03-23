ae522897d819f1c007c738d410f98bbf9ea87d2e
hussein malingha <husseinmalingha@osep.local>
2020-03-23 21:58:46 +0300

    Made migrations, updated models and views files.

--- cms/blog/views.py
+++ cms/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Osep',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'March 23rd, 2020'
-    },
-    {
-        'author': 'Lul',
-        'title': 'Blog Post 2',
-        'content': 'Second post',
-        'date_posted': 'March 24th, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
